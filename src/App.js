029f913b2efbf25ba1c6d5f688093ace5ff845b8
kiakayemen <89444353+kiakayemen@users.noreply.github.com>
2021-11-07 13:35:06 +0330

    personalize 2
    footer

--- src/App.js
+++ src/App.js
@@ -16,13 +16,15 @@ class App extends Component {
       <div className="wrapper">
         <div className="center-wrapper">
           <h1>KI KOON MIDE?</h1>
-          <h2> Made By Kia</h2>
           {this.context.state.stage === 1 ?
             <Stage1 />
             :
             <Stage2/>
           }
         </div>
+          <footer>
+            <h4>Made By Kia</h4>
+          </footer>
       </div>
     );
   }
